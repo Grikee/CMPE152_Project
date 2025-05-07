6519629df1be7087925c8d7442ca71c5ce5e9268
Jeric <jmontepalco@gmail.com>
2025-05-06 23:56:24 -0700

    Main implemnetation

--- output.cpp
+++ output.cpp
@@ -1,5 +1,9 @@
 int main() {
   int x = 10 ;
-  int y = 10 - 2 * 4 / 2 ;
-  return y;
+  int y = 5 ;
+  int z = ( x + y ) * 2 ;
+  int a = ( x * y + z ) / 2 ;
+  int b = x - y + a * 3 ;
+  int c = ( x + y ) * ( z - a ) + b / 4 ;
+  return c;
 }
